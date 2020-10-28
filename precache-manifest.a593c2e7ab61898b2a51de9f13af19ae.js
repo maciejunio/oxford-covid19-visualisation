b9b3636a7f1631a797cfd25181229bf86dee946b
Maciek Górnicki <maciej.gornicki@agora.pl>
2020-10-29 00:01:48 +0100

    Initial dist subtree commit

---
+++ precache-manifest.a593c2e7ab61898b2a51de9f13af19ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af538a44f87301a35bfe",
+    "url": "/oxford-covid19-visualisation/css/about.c297e864.css"
+  },
+  {
+    "revision": "9c0624c10c0ae2c14bc6",
+    "url": "/oxford-covid19-visualisation/css/app.601d2ada.css"
+  },
+  {
+    "revision": "12806da0e7f14ba4808a",
+    "url": "/oxford-covid19-visualisation/css/chunk-vendors.f32eaac2.css"
+  },
+  {
+    "revision": "9672ef9b98d5d36f1a6d3ae3d19a31d0",
+    "url": "/oxford-covid19-visualisation/index.html"
+  },
+  {
+    "revision": "af538a44f87301a35bfe",
+    "url": "/oxford-covid19-visualisation/js/about.646bc726.js"
+  },
+  {
+    "revision": "9c0624c10c0ae2c14bc6",
+    "url": "/oxford-covid19-visualisation/js/app.7156d62c.js"
+  },
+  {
+    "revision": "12806da0e7f14ba4808a",
+    "url": "/oxford-covid19-visualisation/js/chunk-vendors.111ae5cf.js"
+  },
+  {
+    "revision": "0338aa6415d7682d6a382452a556210e",
+    "url": "/oxford-covid19-visualisation/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/oxford-covid19-visualisation/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/oxford-covid19-visualisation/precache-manifest.a593c2e7ab61898b2a51de9f13af19ae.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "oxford-covid19-visualisation"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
